ef9ee6517b600948f6d2ca0701f54ad01744efda
Vicky John <vicky.d.john@email.com>
2023-10-17 15:10:08 -0700

    add methods to create new sub

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,8 +3,27 @@ class SubscriptionsController < ApplicationController
   end
 
   def new
+    @subscription = Subscription.new
+  end
+
+  def create
+    @subscription = Subscription.new(subscription_params)
+
+    if @subscription.save
+      flash[:notice] = 'Subscription successfully added'
+      redirect_to subscriptions_path
+    else
+      flash[:notice] = 'Subscription successfully not added'
+      redirect_to 'new'
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:name, :cost, :frequency, :renewal_date)
   end
 end
