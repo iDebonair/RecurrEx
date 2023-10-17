07a13212a08cc3439e63010ecbeac3cd331ad081
iDebonair <ofemiowolabi@gmail.com>
2023-10-17 09:50:03 -0600

    Corrected the user route to reflect user ID

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ include SessionsHelper
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to their profile or a different page
       log_in user
-      redirect_to home_index_logged_in_path
+      redirect_to user_path(current_user.id)
     else
       # Display an error message and render the login form again
       flash.now[:danger] = 'Invalid email/password combination'
