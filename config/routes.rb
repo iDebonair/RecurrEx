c8aee854e288b2e193eb7c4ce61c8e1c69761c49
Vicky John <vicky.d.john@email.com>
2023-10-17 15:13:09 -0700

    add new sub routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'subscriptions/new'
   get 'subscriptions/edit'
 
-  resources :subscriptions
+  resources :subscriptions, only: [:new, :create, :index]
   resources :users, only: [:new, :create, :edit, :update]
 
   get '/signup', to: 'users#new'
