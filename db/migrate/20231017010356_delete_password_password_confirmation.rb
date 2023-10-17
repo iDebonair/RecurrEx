e2d157ec2218a1ab247bbbad7937be2398ca7279
iDebonair <ofemiowolabi@gmail.com>
2023-10-16 19:24:50 -0600

    Created home route for non-signed in users

--- db/migrate/20231017010356_delete_password_password_confirmation.rb
+++
@@ -1,6 +0,0 @@
-class DeletePasswordPasswordConfirmation < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :password
-    remove_column :users, :password_confirmation
-  end
-end
