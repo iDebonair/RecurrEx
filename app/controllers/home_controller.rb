ccd6d46f13af455601af90273b1aa922b54439b9
Vicky John <vicky.d.john@email.com>
2023-10-18 15:19:38 -0700

    display categories and subscriptions in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
     def index
       user_id = params[:user_id]
     end
+
+    def index_logged_in
+      @categories = Category.all
+      @user_subscriptions = current_user.subscriptions
+    end
   end
   
