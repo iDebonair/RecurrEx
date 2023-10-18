b602bb33c34fd36371d80099ef0a325758fdab99
Vicky John <vicky.d.john@email.com>
2023-10-18 15:12:56 -0700

    add category feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Categories
+Category.create(name: 'Entertainment')
+Category.create(name: 'Fitness and Health')
+Category.create(name: 'Shopping')
+Category.create(name: 'Food and Dining')
