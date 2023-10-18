c404b1336bcc19e0fbb90a6fe48d0dd71fe0f70c
iDebonair <ofemiowolabi@gmail.com>
2023-10-17 18:01:31 -0600

    Modified new user route

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :password, presence: true, confirmation: true
 
-    attr_accessor :password_confirmation
   
   
     has_secure_password
