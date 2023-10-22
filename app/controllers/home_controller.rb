b18f9131915f74e363b447431f60549572e179d5
Vicky John <vicky.d.john@email.com>
2023-10-21 19:42:43 -0700

    modified displayed categories to show recent subscriptions instead

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,48 +7,7 @@ class HomeController < ApplicationController
   end
 
   def index_logged_in
-    @categories = Category.all
-    @user_subscriptions = current_user.subscriptions
-    @subscriptions = Subscription.all
-
-    # Initialize an empty hash to store the total cost for each category
-    @category_costs = {}
-
-    # Retrieve the start_date and end_date from the user's input (update these based on your date picker implementation)
-    start_date = params[:start_date]
-    end_date = params[:end_date]
-
-    @categories.each do |category|
-      # Filter subscriptions for the current category
-      subscriptions_in_category = @subscriptions.where(category:)
-
-      # Initialize the total cost for this category
-      total_cost = 0.0
-
-      # Iterate through subscriptions in the category
-      subscriptions_in_category.each do |subscription|
-        next if subscription.start_date.nil? # Skip subscriptions with nil start_date
-        next if subscription.start_date > end_date || subscription.start_date > start_date
-
-        case subscription.frequency
-        when 'Weekly'
-          # Calculate the number of weeks within the selected date range
-          weeks_within_range = ((end_date - [subscription.start_date, start_date].max).to_i / 7).ceil
-          total_cost += subscription.cost * weeks_within_range
-        when 'Monthly'
-          # Calculate the number of full months within the selected date range
-          months_within_range = (1..12).count do |month|
-            (start_date..end_date).cover?(Date.new(end_date.year, month, 1))
-          end
-          total_cost += subscription.cost * months_within_range
-        when 'Yearly'
-          total_cost += subscription.cost
-        end
-      end
-
-      # Store the total cost for the category
-      @category_costs[category.id] = total_cost
-    end
+    @categories = Category.includes(:subscriptions).order('subscriptions.renewal_date DESC')
   end
 
   private
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,25 +7,5 @@ class Subscription < ApplicationRecord
   validates :renewal_date, presence: true
   validates :cost, presence: true
   validates :frequency, presence: true
-
-  def amount_spent_this_year
-    return 0.0 unless start_date.present?  # Return 0 if start_date is not set
-  
-    today = Date.today
-    end_of_year = Date.new(today.year, 12, 31)  # End of the current year
-  
-    return 0.0 if start_date > end_of_year  # Return 0 if start_date is later than the end of the year
-  
-    # Calculate the total amount based on the frequency
-    case frequency
-    when 'Weekly'
-      (today - start_date).to_i / 7 * cost
-    when 'Monthly'
-      (today.year * 12 + today.month - (start_date.year * 12 + start_date.month)) * cost
-    when 'Yearly'
-      cost
-    else
-      0
-    end
-  end
+  validates :start_date, presence: true
 end
