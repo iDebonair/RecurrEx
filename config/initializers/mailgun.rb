b50fbe5bc29b4ef0c087f3e5bbdc902e9913f79c
Vicky John <vicky.d.john@email.com>
2023-10-28 09:59:54 -0700

    hide env keys

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,4 +1,4 @@
 Mailgun.configure do |config|
-  config.api_key = '08ce5da9720592b9b3f77bf586206f54-324e0bb2-bcee280a'
-  config.domain  = 'sandboxab8ef21068ec4729a062ca9dc248e2cd.mailgun.org'
+  config.api_key = ENV['MAILGUN_API_KEY']
+  config.domain  = ENV['MAILGUN_DOMAIN']
 end
