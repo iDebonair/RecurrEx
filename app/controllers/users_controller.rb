7d33f77c855f9552cbc67e566349ac41e06c6346
iDebonair <ofemiowolabi@gmail.com>
2023-11-05 01:32:37 -0600

    Modify user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :current_password, :new_password)
+    params.require(:user).permit(:first_name, :last_name, :email, :current_password, :new_password, :password, :password_confirmation)
   end
 
   def require_login
