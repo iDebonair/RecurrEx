4948180041c6f32984ab5bd12c6465829b14876e
iDebonair <ofemiowolabi@gmail.com>
2023-11-05 19:49:53 -0700

    Handled cancel button in profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,73 +1,64 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:new, :create]
-  before_action :set_user, only: [:edit, :update]
+    before_action :require_login, except: [:new, :create]
+    before_action :set_user, only: [:edit, :update]
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id # Log in the user upon successful signup
-      redirect_to user_profile_path(@user)
-    else
-      if @user.errors[:password].include?("is too short (minimum is 6 characters)")
-        flash[:error] = "Password must be at least 6 characters long."
-      end
-      render 'new'
+    def new
+      @user = User.new
     end
-  end
 
-  def edit
-    # @user is already set by the before_action
-  end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id # Log in the user upon successful signup
+        redirect_to user_profile_path(@user)
+      else
+        if @user.errors[:password].include?("is too short (minimum is 6 characters)")
+          flash[:error] = "Password must be at least 6 characters long."
+        end
+        render 'new'
+      end
+    end
+
+    def edit
+      # @user is already set by the before_action
+    end
 
   def update
     @user = current_user
-  
-    if user_params[:first_name].present?
-      @user.first_name = user_params[:first_name]
-    end
-  
-    if user_params[:last_name].present?
-      @user.last_name = user_params[:last_name]
-    end
-  
+
     if user_params[:current_password].present?
-      if user_params[:current_password] == user_params[:password]
-        flash[:alert] = "Current password and new password must be different."
-        render 'edit'
-        return
-      end
-  
-      if @user.authenticate(user_params[:current_password])
-        if user_params[:new_password].present? && user_params[:new_password].length >= 6
-          @user.password = user_params[:new_password]
-          @user.password_confirmation = user_params[:new_password]
-          flash[:notice] = "Password updated successfully."
-        else
-          flash[:alert] = "New password must be at least 6 characters long."
-          render 'edit'
-          return
-        end
-      else
+      unless @user.authenticate(user_params[:current_password])
         flash[:alert] = "Current password is incorrect."
-        render 'edit'
-        return
+        return render 'edit'
+      end
+
+      if user_params[:new_password].present? && user_params[:new_password].length >= 6
+        @user.password = user_params[:new_password]
+        @user.password_confirmation = user_params[:new_password]
+        flash[:notice] = "Password updated successfully."
+        return render 'edit'
+      else
+        flash[:alert] = "New password must be at least 6 characters long."
+        return render 'edit'
       end
     end
-  
-    if @user.update(user_params.except(:current_password, :new_password))
-      flash[:notice] = "Profile updated successfully."
-    else
-      flash[:alert] = "Failed to update profile."
-      render 'edit'
+
+    if user_params[:first_name].present? || user_params[:last_name].present?
+      if @user.update(user_params.slice(:first_name, :last_name))
+        flash[:notice] = "Profile updated successfully."
+      else
+        flash[:alert] = "Failed to update profile: " + @user.errors.full_messages.join(', ')
+      end
     end
+
+    if user_params[:current_password].blank? && user_params[:first_name].blank? && user_params[:last_name].blank?
+      flash[:alert] = "No changes were made."
+      return render 'edit'
+    end
+
+    redirect_to user_profile_path(@user)
   end
-  
-  
-  
+
   
   
   
